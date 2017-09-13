a5c3b870f9db10965e551e7fa859a7875b9a8f95
treza <trezabawmwin@gmail.com>
2017-09-13 22:55:41 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/controller/ControlFactory.java
@@ -0,0 +1,25 @@
+package sg.edu.nus.iss.phoenix.core.android.controller;
+
+import sg.edu.nus.iss.phoenix.authenticate.android.controller.LoginController;
+import sg.edu.nus.iss.phoenix.radioprogram.android.controller.ProgramController;
+
+public class ControlFactory {
+    private static MainController mainController = null;
+    private static LoginController loginController = null;
+    private static ProgramController programController = null;
+
+    public static MainController getMainController() {
+        if (mainController == null) mainController = new MainController();
+        return mainController;
+    }
+
+    public static LoginController getLoginController() {
+        if (loginController == null) loginController = new LoginController();
+        return loginController;
+    }
+
+    public static ProgramController getProgramController() {
+        if (programController == null) programController = new ProgramController();
+        return programController;
+    }
+}
---
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/controller/MainController.java
@@ -0,0 +1,50 @@
+package sg.edu.nus.iss.phoenix.core.android.controller;
+
+import android.app.Application;
+import android.content.Intent;
+
+import sg.edu.nus.iss.phoenix.core.android.ui.MainScreen;
+
+public class MainController {
+    private static Application app = null;
+    private String username;
+    private MainScreen mainScreen;
+
+    public static Application getApp() {
+        return app;
+    }
+
+    public static void setApp(Application app) {
+        MainController.app = app;
+    }
+
+    public static void displayScreen(Intent intent) {
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        app.startActivity(intent);
+    }
+
+    public void startUseCase(String username) {
+        this.username = username;
+
+        Intent intent = new Intent(MainController.getApp(), MainScreen.class);
+        MainController.displayScreen(intent);
+    }
+
+    public void onDisplay(MainScreen mainScreen) {
+        this.mainScreen = mainScreen;
+        mainScreen.showUsername(username);
+    }
+
+    public void selectMaintainProgram() {
+        ControlFactory.getProgramController().startUseCase();
+    }
+
+    public void maintainedProgram() {
+        startUseCase(username);
+    }
+
+    public void selectLogout() {
+        username = "<not logged in>";
+        ControlFactory.getLoginController().logout();
+    }
+}
