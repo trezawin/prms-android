ef6e200452ec55df5a5058f862f56d3573633e7b
treza <trezabawmwin@gmail.com>
2017-10-01 18:29:57 +0800

    Merge with latest

--- app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/delegate/CreateScheduleDelegate.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/delegate/CreateScheduleDelegate.java
@@ -52,11 +52,11 @@ public class CreateScheduleDelegate extends AsyncTask<ProgramSlot, Void, Boolean
             programSlotJson.put("dateOfProgramTimestamp", params[0].getDuration().getTime());
             programSlotJson.put("assignedBy", params[0].getAssignedBy());
 
-            programSlotJson.put("presenterId", params[0].getPresenterId());
-            programSlotJson.put("presenterName", params[0].getPresenterName());
-            programSlotJson.put("producerId", params[0].getProducerId());
-            programSlotJson.put("producerName", params[0].getProducerName());
-            programSlotJson.put("radioProgramName", params[0].getRadioProgramName());
+//            programSlotJson.put("presenterId", params[0].getPresenterId());
+//            programSlotJson.put("presenterName", params[0].getPresenterName());
+//            programSlotJson.put("producerId", params[0].getProducerId());
+//            programSlotJson.put("producerName", params[0].getProducerName());
+//            programSlotJson.put("radioProgramName", params[0].getRadioProgramName());
 
         } catch (JSONException e) {
             Log.v(TAG, e.getMessage());
--- app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/delegate/RetrieveScheduleDelegate.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/schedule/android/delegate/RetrieveScheduleDelegate.java
@@ -79,7 +79,6 @@ public class RetrieveScheduleDelegate extends AsyncTask<String, Void, String> {
 
     public void processResponse(String result, List<ProgramSlot> programSlots) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ"); // eg; 1970-01-01T00:12:34+07:30
-//        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         if (result != null && !result.equals("")) {
             try {
@@ -95,11 +94,12 @@ public class RetrieveScheduleDelegate extends AsyncTask<String, Void, String> {
                     ps.setProgramName(rpJson.getString("programName"));
                     ps.setDuration(sdf.parse(duration));
                     ps.setDateOfProgram(sdf.parse(dateOfProgram));
-                    ps.setPresenterId(rpJson.getString("presenterId"));
-                    ps.setPresenterName(rpJson.getString("presenterName"));
-                    ps.setProducerId(rpJson.getString("producerId"));
-                    ps.setProducerName(rpJson.getString("producerName"));
-                    ps.setRadioProgramName(rpJson.getString("radioProgramName"));
+
+//                    ps.setPresenterId(rpJson.getString("presenterId"));
+//                    ps.setPresenterName(rpJson.getString("presenterName"));
+//                    ps.setProducerId(rpJson.getString("producerId"));
+//                    ps.setProducerName(rpJson.getString("producerName"));
+//                    ps.setRadioProgramName(rpJson.getString("radioProgramName"));
 
                     //TODO : to remove comment
 //                    ps.setAssignedBy((rpJson.getString("assignedBy") == null? "" : rpJson.getString("assignedBy")));
